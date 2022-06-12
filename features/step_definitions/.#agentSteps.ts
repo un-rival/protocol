324a86cb8bdba5ecee20f5f935e6d0b34b1069fa
David Joseph <david.joseph@passcon.de>
2022-06-12 12:39:28 +0200

    add steps

---
+++ features/step_definitions/.#agentSteps.ts
@@ -0,0 +1,1 @@
+davidjoseph@DEMLFVFHC0KTQ05N.28665
---
+++ features/step_definitions/Agent.steps.ts
@@ -0,0 +1,8 @@
+
+import {Given, When, Then} from "@cucumber/cucumber"
+
+
+Given('a public key', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
